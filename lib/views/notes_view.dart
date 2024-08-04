3b88f9eac7367f55dbae4e3545759b21889c1f65
ahmedElmasry96 <ahmedmasrey9@gmail.com>
2024-08-04 23:30:23 +0300

    list view notes

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar_widget.dart';
+import 'package:notes_app/views/widgets/custom_note_item_widget.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
-            SizedBox(height: 45,),
-            CustomAppBarWidget(),
+            const SizedBox(
+              height: 45,
+            ),
+            const CustomAppBarWidget(),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.only(bottom: 12),
+                  child: CustomNoteItemWidget(),
+                );
+              }),
+            ),
           ],
         ),
       ),
---
+++ lib/views/widgets/custom_note_item_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+class CustomNoteItemWidget extends StatelessWidget {
+  const CustomNoteItemWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 20, bottom: 20, left: 10, right: 0),
+      decoration: BoxDecoration(
+        color: const Color(0xffE67E22),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Padding(
+              padding: EdgeInsets.only(bottom: 15),
+              child: Text("Flutter Tips", style: TextStyle(color: Colors.black, fontSize: 24),),
+            ),
+            subtitle: Text("Build your career with Ahmed Elmasry", style: TextStyle(color: Colors.black.withOpacity(.5), fontSize: 16),),
+            trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.delete, color: Colors.black,), iconSize: 35,),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 15, top: 20),
+            child: Text("Aug 4, 2024", style: TextStyle(color: Colors.black.withOpacity(0.4)),),
+          )
+        ],
+      ),
+    );
+  }
+}
