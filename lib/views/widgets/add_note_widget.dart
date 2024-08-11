e12d571f4bcce62b7542b65525df982574e0d14f
ahmedElmasry96 <ahmedmasrey9@gmail.com>
2024-08-11 22:42:41 +0300

    bloc consumer

---
+++ lib/views/widgets/add_note_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'custom_button_widget.dart';
+import 'custom_text_field_widget.dart';
+
+class AddNoteWidget extends StatefulWidget {
+  const AddNoteWidget({super.key});
+
+  @override
+  State<AddNoteWidget> createState() => _AddNoteWidgetState();
+}
+
+class _AddNoteWidgetState extends State<AddNoteWidget> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autoValidateMode,
+      child: Column(
+        children: [
+          CustomTextFieldWidget(
+            hintText: "Title",
+            onSaved: (value) {
+              title = value;
+            }, validator: (value) {
+            if(value?.isEmpty ?? true) {
+              return "Title is required";
+            }
+            return null;
+          },
+
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomTextFieldWidget(
+            maxLines: 5,
+            hintText: "Body",
+            onSaved: (value) {
+              subTitle = value;
+            },
+            validator: (value) {
+              if(value?.isEmpty ?? true) {
+                return "SubTitle is required";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          CustomBottomWidget(
+              onTap: () {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                } else {
+                  autoValidateMode = AutovalidateMode.always;
+                  setState(() {
+
+                  });
+                }
+              }
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/modal_bottom_sheet_widget.dart
+++ lib/views/widgets/modal_bottom_sheet_widget.dart
@@ -1,83 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_button_widget.dart';
-import 'package:notes_app/views/widgets/custom_text_field_widget.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/views/widgets/add_note_widget.dart';
 
 class ModalBottomSheetWidget extends StatelessWidget {
   const ModalBottomSheetWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        child: AddNoteWidget(),
-      ),
-    );
-  }
-}
-
-class AddNoteWidget extends StatefulWidget {
-  const AddNoteWidget({super.key});
-
-  @override
-  State<AddNoteWidget> createState() => _AddNoteWidgetState();
-}
-
-class _AddNoteWidgetState extends State<AddNoteWidget> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
-  String? title, subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autoValidateMode,
-      child: Column(
-        children: [
-          CustomTextFieldWidget(
-            hintText: "Title",
-            onSaved: (value) {
-              title = value;
-            }, validator: (value) {
-              if(value?.isEmpty ?? true) {
-                return "Title is required";
-              }
-              return null;
-          },
-
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          CustomTextFieldWidget(
-            maxLines: 5,
-            hintText: "Body",
-            onSaved: (value) {
-              subTitle = value;
-            },
-            validator: (value) {
-              if(value?.isEmpty ?? true) {
-                return "SubTitle is required";
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          CustomBottomWidget(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autoValidateMode = AutovalidateMode.always;
-                setState(() {
-
-                });
-              }
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print("error ${state.errMessage}");
             }
-            ),
-        ],
+
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: const AddNoteWidget(),
+            );
+          },
+        ),
       ),
     );
   }
